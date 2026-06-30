cfe20cb57bc9ea2a34ddb3109197c97fabae7adc
mohitpal24 <mohitmusic2429@gmail.com>
2026-07-01 01:48:00 +0530

    Fix ALLOWED_HOSTS: default to * so Render works without explicit env var

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-s5#j$m1xhx75kjsas1r5#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
 
